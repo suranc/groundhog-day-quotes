86c91c63f383ac1dd4543017a4a852bf57313dc5
Chris Suran <suran.c@gmail.com>
2019-02-21 15:49:57 -0500

    Adding more quotes

--- init-db/quotes.js
+++ init-db/quotes.js
@@ -1,1 +1,16 @@
-db.quotes.insert({quote:"Watch out for that first step!  It's a doozy!"}); 
+db.quotes.insert({quote:"Watch out for that first step!  It's a doozy!"}); db.quotes.insert({quote:"Bing!"});
+db.quotes.insert({quote:"SIX!"});
+db.quotes.insert({quote:"Rise and shine woodchuck chuckers it's groundhog day!"});
+db.quotes.insert({quote:"Chapped lips"});
+db.quotes.insert({quote:"I studied 19th century poretry."});
+db.quotes.insert({quote:"You told me to stay, so I stayed!"});
+db.quotes.insert({quote:"It's going to be cold.  It's going to be grey.  It's going to last for the rest of your life."});
+db.quotes.insert({quote:"He's out of his gord!"});
+db.quotes.insert({quote:"For your information HAIRDOO!  There's a major news network interested in me."});
+db.quotes.insert({quote:"Yeah, the home shopping network."});
+db.quotes.insert({quote:"My old fiance.  Doesn't even remember me."});
+db.quotes.insert({quote:"I told you, call me Bronco."});
+db.quotes.insert({quote:"Phil."});
+db.quotes.insert({quote:"Phil!?  Like the groundhog Phil!?"});
+db.quotes.insert({quote:"Who else could go for some flapjacks right now?"});
+db.quotes.insert({quote:"Too early for flapjacks?"});
